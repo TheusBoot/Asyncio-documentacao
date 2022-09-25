3760aeef3092ed3c95bb413f6e8eec9cab316c13
Matheus Trindade <emailmartingcf1@gmail.com>
2022-09-25 06:40:05 -0300

    simples

--- sintaxe_base.py
+++ sintaxe_base.py
@@ -14,10 +14,17 @@ loop.close()
 
 #---------------------------------------------------------------
 
-@coroutine
-def print_async(msg:str) -> None:
+
+async def print_async(msg:str) -> None:
     print(f'ASYNC:{msg}')
 
 loop = get_event_loop()
 loop.run_until_complete(print_async('OI'))
 loop.close()
+
+# SAIDA    OI
+
+#------------------------------------------------------------------
+
+
+
