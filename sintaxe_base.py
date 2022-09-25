f2c6cf67d12e208d0754d0190560ed4b19582adb
Matheus Trindade <emailmartingcf1@gmail.com>
2022-09-25 06:31:53 -0300

    código base

--- sintaxe_base.py
+++ sintaxe_base.py
@@ -1,1 +1,23 @@
-c
+#import asyncio
+from asyncio import get_event_loop, coroutine
+
+
+@coroutine
+def print_async(msg:str) -> None:
+    print(f'ASYNC:{msg}')
+
+loop = get_event_loop()
+loop.run_until_complete(print_async('OI'))
+loop.close()
+
+#SAIDA   OI
+
+#---------------------------------------------------------------
+
+@coroutine
+def print_async(msg:str) -> None:
+    print(f'ASYNC:{msg}')
+
+loop = get_event_loop()
+loop.run_until_complete(print_async('OI'))
+loop.close()
