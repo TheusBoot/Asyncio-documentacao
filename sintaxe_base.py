998f2b0b792c8aab85ee9248e0fbf541bcafcd43
Matheus Trindade <emailmartingcf1@gmail.com>
2022-09-25 06:51:55 -0300

    finally

--- sintaxe_base.py
+++ sintaxe_base.py
@@ -35,5 +35,19 @@ loop = get_event_loop()
 loop.run_until_complete(print_async('OI'))
 loop.close()
 
+#------------------------------------------------------------------
+
+
+async def print_async(path):
+    await ping_server('127.0.0.1') 
+    #await == aguardar até que essa tarefa seja concluída.
+
+    #sintaxe base (async só vai ser usado com await)
+
+loop = get_event_loop()
+loop.run_until_complete(print_async('OI'))
+loop.close()
+
+
 
 
